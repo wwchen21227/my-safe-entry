4e2d0086fd9ce8d8044f07d7a7433c5aba299b29
Chen Weng Wei <wwchen21227@gmail.com>
2020-08-16 17:39:33 +0800

    1) Added searchbox. 2) Visit button

--- index.js
+++ index.js
@@ -18,20 +18,48 @@ const toggleClass = (elem, className) => {
     }
 };
 
+const sortEntryByDate = (arr) => {
+    return arr.sort((a, b) => b.lastVisitDate - a.lastVisitDate);
+}
+
+const searchTenant = (arr, keyword) => {
+    const regex = new RegExp(keyword, 'i');
+    return arr.filter(entry => regex.test(entry.tenant));
+};
+
+const EntryStore = (listKey) => {
+    const save = (entries) => {
+        localforage.setItem(listKey, entries)
+        .then(value => console.log(value))
+        .catch(function (err) {
+            console.error(err);
+        });
+    };
+
+    const getAllEntries = () => {
+        return localforage.getItem(listKey);
+    }
+
+    return {
+        save,
+        getAllEntries
+    }
+};
+
 (function() {
     'use strict';
 
-    const DB_KEY = 'entries-list';
-    let entryList = [];
+    const LIST_KEY = 'entries-list';
+    const entryStore = EntryStore(LIST_KEY);
 
-    QrScanner.hasCamera().then(hasCamera => console.log(`Camera detected ${hasCamera}`));
+    let entryList = [];
 
     const scanContainer = document.getElementById('scanContainer');
     const qrScannerContainer = document.getElementById('qrScannerContainer');
     const entryListContainer = document.getElementById('entryListContainer');
     const entryListElem = document.getElementById('entryList');
     const btnQrScan = document.getElementById('btnQrScan');
-
+    const txtSearchBox = document.getElementById('txtSearchBox');
     const qrVideo = document.getElementById('qrVideo');
     const camQrResult = document.getElementById('cam-qr-result');
 
@@ -50,18 +78,54 @@ const toggleClass = (elem, className) => {
                 `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
     }
 
-    localforage.getItem(DB_KEY).then(data => {
-        if(data && data.length > 0) {
-            entryList = data;
+    const updateVisitEntry = (key) => {    
+        let entry = entryList.find(entry => entry.key === key);
+        entry.lastVisitDate = new Date();
+        entry.visits += 1;
 
-            let list = ``;
-            data.forEach(entry => {
-                list += `<li>` + 
-                            `<a href="${entry.url}" target="_blank">${entry.tenant}</a>` +
-                            `<div><span class="entry-date">Last visit: ${formatDate(entry.lastVisitDate)}</span></div>` + 
-                        `</li>`
+        const sortedList = sortEntryByDate(entryList);
+
+        entryStore.save(sortedList);
+        buildEntryListElem(sortedList);
+    };
+
+    const handleVisitClick = (e) => {
+        updateVisitEntry(e.target.dataset.key);
+    };
+
+    const bindListEvents = (list) => {
+        const buttons = list.querySelectorAll('.btn-visit');
+        buttons.forEach(button => button.addEventListener('click', handleVisitClick));
+    };
+
+    const buildEntryListItemElem = entry => {
+        return `<li>` + 
+                    `<div><span class="entry-title">${entry.tenant}</span>` +
+                    `<div><span class="entry-date">Last visit: ${formatDate(entry.lastVisitDate)}</span></div></div>` + 
+                    `<a class="btn btn-secondary btn-visit" href="${entry.url}" data-key="${entry.key}" target="_blank">Visit</a>` + 
+                `</li>`;
+    };
+
+    const buildEntryListElem = entries => {
+        let list = ``;
+        if(entries.length > 0)  {
+            entries.forEach(entry => {
+                list += buildEntryListItemElem(entry);
             });
             entryListElem.innerHTML = list;
+            bindListEvents(entryListElem);
+        }
+        else {
+            entryListElem.innerHTML = '<li>No result found.</li>';
+        }
+    }
+
+    entryStore.getAllEntries().then(data => {
+        if(data && data.length > 0) {
+            entryList = sortEntryByDate(data);
+
+            buildEntryListElem(entryList);
+            
             toggleClass(entryListContainer, 'hide');
         }else {
             toggleClass(scanContainer, 'hide');
@@ -70,32 +134,23 @@ const toggleClass = (elem, className) => {
 
     let cacheKey = '';
     function setResult(label, result) {
-        console.log(result);
         const url = new URL(result);
         const tenantKey = url.pathname.split('/')[2];
 
         if(cacheKey !== tenantKey) {
             cacheKey = tenantKey;
-
-            console.log(getTenantName(tenantKey));
             const isExist = entryList.some(entry => entry.key === tenantKey);
 
             if(!isExist) {
-                const entry = {
+                entryList.push({
                     key: tenantKey,
                     tenant: getTenantName(tenantKey),
                     url: result,
                     lastVisitDate: new Date(),
                     visits: 1
-                };
-
-                entryList.push(entry);
-    
-                localforage.setItem(DB_KEY, entryList)
-                .then(value => console.log(value))
-                .catch(function (err) {
-                    console.error(err);
                 });
+    
+                entryStore.save(entryList);
             }
 
             toggleClass(entryListContainer, 'hide');
@@ -103,11 +158,12 @@ const toggleClass = (elem, className) => {
         }
     }
 
+    QrScanner.hasCamera().then(hasCamera => console.log(`Camera detected ${hasCamera}`));
     const qrScanner = new QrScanner(qrVideo, result => setResult(camQrResult, result));
 
     document.getElementById('start-button').addEventListener('click', () => {
         qrScanner.start().then(stream => {
-            console.log(stream);
+            //console.log(stream);
             toggleClass(scanContainer, 'hide');
             toggleClass(qrScannerContainer, 'hide');
 
@@ -138,5 +194,15 @@ const toggleClass = (elem, className) => {
 
             document.getElementById('canvas').appendChild(qrScanner.$canvas);
         });
+    });
+
+    let timeoutId = null;
+    txtSearchBox.addEventListener('keyup', (e) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            const filteredList = searchTenant(entryList, e.target.value);
+            buildEntryListElem(filteredList);
+            console.log('fired')
+        }, 300);
     });
 })();
