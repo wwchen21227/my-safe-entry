ceab0b8bebd8e5671e33ba094eb40bd2ae5165ef
Chen Weng Wei <wwchen21227@gmail.com>
2020-08-19 12:44:24 +0800

    Clean up js and add rename overlay

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-const addClass = (elem, className) => {
-    elem.classList.add(className);
-};
 
-const removeClass = (elem, className) => {
-    elem.classList.remove(className);
-};
+/*** Util function ***/
+const sortEntryByDate = (arr) => arr.sort((a, b) => b.lastVisitDate - a.lastVisitDate);
 
-const hasClass = (elem, className) => {
-    return elem.classList.contains(className);
-};
-
-const toggleClass = (elem, className) => {
-    if (hasClass(elem, className)) {
-        removeClass(elem, className);
-    }else {
-        addClass(elem, className);
+const getTenantName = key => {
+    const arr = key.split('-');
+    let tenantName = arr[2];
+    for(let i = 3; i < arr.length - 1; i++) {
+        tenantName += ' ' + arr[i];
     }
+    return tenantName;
 };
-
-const sortEntryByDate = (arr) => {
-    return arr.sort((a, b) => b.lastVisitDate - a.lastVisitDate);
-}
 
 const searchTenant = (arr, keyword) => {
     const regex = new RegExp(keyword, 'i');
@@ -32,6 +21,24 @@ const formatDate = (value) => {
     return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()} ` + 
             `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
+/*** End Util function ***/
+
+/*** CSS class Util ***/
+
+const CssClass = {
+    addClass: (elem, className) => elem.classList.add(className),
+    removeClass: (elem, className) => elem.classList.remove(className),
+    hasClass: (elem, className) => elem.classList.contains(className),
+    toggleClass: (elem, className) => {
+        if (CssClass.hasClass(elem, className)) {
+            CssClass.removeClass(elem, className);
+        }else {
+            CssClass.addClass(elem, className);
+        }
+    }
+};
+
+/*** End CSS class Util ***/
 
 const EntryStore = (listKey) => {
     const save = (entries) => {
@@ -52,60 +59,13 @@ const EntryStore = (listKey) => {
     }
 };
 
-(function() {
-    'use strict';
+/*** Building entry list ***/
 
-    const LIST_KEY = 'entries-list';
-    const entryStore = EntryStore(LIST_KEY);
-
-    let entryList = [];
-
-    const scanContainer = document.getElementById('scanContainer');
-    const qrScannerContainer = document.getElementById('qrScannerContainer');
-    const entryListContainer = document.getElementById('entryListContainer');
-    const entryListElem = document.getElementById('entryList');
-    const btnQrScan = document.getElementById('btnQrScan');
-    const txtSearchBox = document.getElementById('txtSearchBox');
-    const qrVideo = document.getElementById('qrVideo');
-    const camQrResult = document.getElementById('cam-qr-result');
-        
-    const stopVideo = () => {
-        qrVideo.pause();
-
-        const tracks = qrVideo.srcObject ? qrVideo.srcObject.getTracks() : [];
-        for (const track of tracks) {
-            track.stop(); 
-        }
-        
-        qrVideo.srcObject = null;
-    };
-
-    const getTenantName = key => {
-        const arr = key.split('-');
-        let tenantName = arr[2];
-        for(let i = 3; i < arr.length - 1; i++) {
-            tenantName += ' ' + arr[i];
-        }
-        return tenantName;
-    };
-
-    
-
-    const updateVisitEntry = (key) => {    
-        let entry = entryList.find(entry => entry.key === key);
-        entry.lastVisitDate = new Date();
-        entry.visits += 1;
-
-        const sortedList = sortEntryByDate(entryList);
-
-        entryStore.save(sortedList);
-        buildEntryListElem(sortedList);
-    };
-
-    const handleVisitClick = (e) => {
-        updateVisitEntry(e.target.dataset.key);
-    };
-
+const UIBuilder = ({
+    entryListElem,
+    handleVisitClick
+}) => {
+   
     const bindListEvents = (list) => {
         const buttons = list.querySelectorAll('.btn-visit');
         buttons.forEach(button => button.addEventListener('click', handleVisitClick));
@@ -140,126 +100,246 @@ const EntryStore = (listKey) => {
         }
     }
 
-    entryStore.getAllEntries().then(data => {
-        if(data && data.length > 0) {
-            entryList = sortEntryByDate(data);
+    return {
+        buildEntryListElem
+    }
+};
 
-            buildEntryListElem(entryList);
-            
-            toggleClass(entryListContainer, 'hide');
-        }else {
-            toggleClass(scanContainer, 'hide');
-            toggleClass(btnQrScan, 'hide');
+/*** End Building entry list ***/
+
+const QRScanner = ({
+    video,
+    canvas,
+    entryStore,
+    entryList,
+    uiBuilder,
+    page
+}) => {
+    const canvasContext = canvas.getContext("2d");
+
+    const check = () => {
+        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+            const smallestDimension = Math.min(video.videoWidth, video.videoHeight);
+            const scanRegionSize = Math.round(2 / 3 * smallestDimension);
+
+            canvas.height = scanRegionSize;
+            canvas.width = scanRegionSize;
+
+            canvasContext.drawImage(qrVideo, 0, 0, canvas.width, canvas.height);
+
+            const imageData = canvasContext.getImageData(0, 0, canvas.width, canvas.height);
+
+            const code = jsQR(imageData.data, imageData.width, imageData.height, {
+                inversionAttempts: "dontInvert",
+            });
+
+            if (code) {
+                cancelAnimationFrame(check);
+                stopScan();
+                setResult(code.data);
+            }
         }
-    });
+        requestAnimationFrame(check);
+    }
 
-    let cacheKey = '';
+    const startScan = (stream, callback) => {
+        video.srcObject = stream;
+        video.play();
+        
+        requestAnimationFrame(check);
+
+        callback();
+    };
+
+    const stopScan = () => {
+        video.pause();
+
+        const tracks = video.srcObject ? video.srcObject.getTracks() : [];
+        for (const track of tracks) {
+            track.stop(); 
+        }
+        
+        video.srcObject = null;
+    };
+
     function setResult(result) {
         const url = new URL(result);
-        const tenantKey = url.pathname.split('/')[2];
+        page.showOverlay(url);
+    }
 
-        if(cacheKey !== tenantKey) {
-            cacheKey = tenantKey;
+    return {
+        startScan,
+        stopScan
+    };
+};
+
+(function() {
+    'use strict';
+    
+    const LIST_KEY = 'entries-list';
+    const entryStore = EntryStore(LIST_KEY);
+    
+    let entryList = [];
+    let timeoutId = null;
+
+    const scanContainer = document.getElementById('scanContainer');
+    const qrScannerContainer = document.getElementById('qrScannerContainer');
+    const entryListContainer = document.getElementById('entryListContainer');
+    const entryListElem = document.getElementById('entryList');
+    const btnQrScan = document.getElementById('btnQrScan');
+    const txtSearchBox = document.getElementById('txtSearchBox');
+    const qrVideo = document.getElementById('qrVideo');
+    const btnScan = document.getElementById('btnScan');
+    const btnCancelScan = document.getElementById('btnCancelScan');
+    const overlayContainer = document.getElementById('overlay');
+    const btnSave = document.getElementById('btnSave');
+    const txtTenantName = document.getElementById('txtTenantName');
+    const canvasElem = document.getElementById('canvas');
+
+    const Page = {
+        showLanding: () => {
+            CssClass.addClass(btnQrScan, 'hide');
+            CssClass.addClass(qrScannerContainer, 'hide');
+            
+            CssClass.removeClass(scanContainer, 'hide');
+        },
+        showEntryList: () => {    
+            CssClass.addClass(qrScannerContainer, 'hide');
+            CssClass.addClass(overlayContainer, 'hide');
+            
+            CssClass.removeClass(entryListContainer, 'hide');
+            CssClass.removeClass(btnQrScan, 'hide');
+        },
+        showScan: () => {
+            CssClass.addClass(scanContainer, 'hide');
+            CssClass.addClass(entryListContainer, 'hide');
+            CssClass.addClass(btnQrScan, 'hide');
+    
+            CssClass.removeClass(qrScannerContainer, 'hide');
+        },
+        showOverlay: (url) => {
+            const tenantKey = url.pathname.split('/')[2];
             const isExist = entryList.some(entry => entry.key === tenantKey);
 
             if(!isExist) {
-                entryList.push({
-                    key: tenantKey,
-                    tenant: getTenantName(tenantKey),
-                    url: result,
-                    lastVisitDate: new Date(),
-                    visits: 1
-                });
+                txtTenantName.value = getTenantName(tenantKey);
+                txtTenantName.setAttribute('data-key', tenantKey);
+                txtTenantName.setAttribute('data-url', url);
     
-                entryStore.save(entryList);
-
-                buildEntryListElem(sortEntryByDate(entryList));
-            }
-
-            toggleClass(entryListContainer, 'hide');
-            toggleClass(qrScannerContainer, 'hide');
-            toggleClass(btnQrScan, 'hide');
+                CssClass.addClass(qrScannerContainer, 'hide');
+                CssClass.removeClass(overlayContainer, 'hide');
+            }else {
+                Page.showEntryList();
+            }           
         }
-    }
+    };
 
-    document.getElementById('start-button').addEventListener('click', () => {
+    const updateVisitEntry = (key) => {    
+        let entry = entryList.find(entry => entry.key === key);
+        entry.lastVisitDate = new Date();
+        entry.visits += 1;
+
+        const sortedList = sortEntryByDate(entryList);
+
+        entryStore.save(sortedList);
+        uiBuilder.buildEntryListElem(sortedList);
+    };
+
+    const uiBuilder = UIBuilder({
+         entryListElem,
+         handleVisitClick: (e) => {
+            updateVisitEntry(e.target.dataset.key);
+        }
+    });
+
+    const qrScanner = QRScanner({ 
+        video: qrVideo,
+        canvas: canvasElem,
+        entryStore,
+        entryList,
+        uiBuilder,
+        page: Page
+    });
+
+    const startStream = () => {
         // Use facingMode: environment to attemt to get the front camera on phones
-        navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
-            .then(function(stream) {
-                qrVideo.srcObject = stream;
-                qrVideo.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-                qrVideo.play();
-                
-                requestAnimationFrame(tick);
+        navigator
+            .mediaDevices
+            .getUserMedia({ video: { facingMode: "environment" } })
+            .then((stream) => qrScanner.startScan(stream, Page.showScan));
+    };
 
-                toggleClass(scanContainer, 'hide');
-                toggleClass(qrScannerContainer, 'hide');
+    const bindEvents = () => {
+        const handleScanClick = () => {
+            startStream();
+        };
+    
+        const handleCancleClick = () => {
+            qrScanner.stopScan();        
+            if(entryList.length === 0) {
+                Page.showLanding();
+            }else {
+               Page.showEntryList();
+            }  
+        };
+        
+        const handleSearchKeyup = (e) => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                const filteredList = searchTenant(entryList, e.target.value);
+                uiBuilder.buildEntryListElem(filteredList);
+            }, 300);
+        };
+
+        const handleSaveClick = () => {
+            if(txtTenantName.value.length === 0) {
+                CssClass.addClass(txtTenantName, 'error');
+                txtTenantName.focus();
+                return;
+            }
+            
+            entryList.push({
+                key: txtTenantName.dataset.key,
+                tenant: txtTenantName.value,
+                url: txtTenantName.dataset.url,
+                lastVisitDate: new Date(),
+                visits: 1
             });
-    });
 
-    document.getElementById('stop-button').addEventListener('click', () => {
-        stopVideo();        
+            entryStore.save(entryList);
 
-        if(entryList.length === 0) {
-            toggleClass(scanContainer, 'hide');
-        }else {
-            toggleClass(entryListContainer, 'hide');
-            toggleClass(btnQrScan, 'hide');
-        }
-        toggleClass(qrScannerContainer, 'hide');
+            uiBuilder.buildEntryListElem(sortEntryByDate(entryList));
+            
+            Page.showEntryList();
+        };
 
-        document.getElementById('canvas').innerHTML = '';
-    });
+        [btnScan, btnQrScan].forEach(elem => elem.addEventListener('click', handleScanClick));
 
-    btnQrScan.addEventListener('click', () => {
-        navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
-            .then(function(stream) {
-                qrVideo.srcObject = stream;
-                qrVideo.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-                qrVideo.play();
-                
-                requestAnimationFrame(tick);
+        btnCancelScan.addEventListener('click', handleCancleClick);
+    
+        txtSearchBox.addEventListener('keyup', handleSearchKeyup);
 
-                toggleClass(entryListContainer, 'hide');
-                toggleClass(btnQrScan, 'hide');
-                toggleClass(qrScannerContainer, 'hide');
+        btnSave.addEventListener('click', handleSaveClick);
+    };
+    
+    const init = () => {
+        bindEvents();
+
+        entryStore
+            .getAllEntries()
+            .then(data => {
+                if(data && data.length > 0) {
+                    entryList = sortEntryByDate(data);
+        
+                    uiBuilder.buildEntryListElem(entryList);
+                    
+                    Page.showEntryList();
+                }else {
+                    Page.showLanding();
+                }
             });
-    });
+    };
 
-    let timeoutId = null;
-    txtSearchBox.addEventListener('keyup', (e) => {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => {
-            const filteredList = searchTenant(entryList, e.target.value);
-            buildEntryListElem(filteredList);
-            console.log('fired')
-        }, 300);
-    });
-
-    const canvasElement = document.getElementById('canvas');
-    const canvas = canvasElement.getContext("2d");
-
-    function tick() {
-        if (qrVideo.readyState === qrVideo.HAVE_ENOUGH_DATA) {
-          const smallestDimension = Math.min(qrVideo.videoWidth, qrVideo.videoHeight);
-          const scanRegionSize = Math.round(2 / 3 * smallestDimension);
-
-          canvasElement.height = scanRegionSize;
-          canvasElement.width = scanRegionSize;
-          canvas.drawImage(qrVideo, 0, 0, canvasElement.width, canvasElement.height);
-
-          const imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-          const code = jsQR(imageData.data, imageData.width, imageData.height, {
-            inversionAttempts: "dontInvert",
-          });
-
-          if (code) {
-            cancelAnimationFrame(tick);
-            stopVideo();
-            setResult(code.data);
-          }
-        }
-        requestAnimationFrame(tick);
-      }
+    init();
 })();
 
