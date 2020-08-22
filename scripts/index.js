2d18a02b7cb4d0b0f43896640a1debcbd7465f1e
Chen Weng Wei <wwchen21227@gmail.com>
2020-08-23 00:05:51 +0800

    uncomment the js

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
 /*** Features detection ***/
-// async function registerSW() {
-//     if ('serviceWorker' in navigator) {
-//         try {
-//             await navigator.serviceWorker.register('../sw.js');
-//         } catch (e) {
-//             alert('ServiceWorker registration failed. Sorry about that.');
-//         }
-//     } else {
-//         document.querySelector('.alert').removeAttribute('hidden');
-//     }
-// }
+async function registerSW() {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('../sw.js');
+        } catch (e) {
+            alert('ServiceWorker registration failed. Sorry about that.');
+        }
+    } else {
+        document.querySelector('.alert').removeAttribute('hidden');
+    }
+}
 
 const IsBrowserSupport = {
     WebWorker: typeof (Worker) !== "undefined"
@@ -524,7 +524,7 @@ const QRScanner = ({
         CssClass.removeClass(app, 'loading');
     };
     window.addEventListener('load', () => {
-        //registerSW();
+        registerSW();
         init();
     });
 })();
